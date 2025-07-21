f605185ad5f154a2a019cd4645a68980929943fa
odunsi <opemipoodunsi428@gmail.com>
2025-07-21 10:06:40 +0100

    Basic functional calculator
    Although a little bit buggy, the calculator performs basic arithemetics.
    I will spend the next couple of days fixing the bugs, i just wanted to push

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = function(a,b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 };
 
 const subtract = function(a,b) {
@@ -11,7 +11,7 @@ const multiply = function(a,b) {
 };
 
 const divide = function(a,b) {
-    return (a / b);
+    return Math.round((a / b) * 100) / 100;
 }
 
 let firstNumber;
@@ -23,15 +23,67 @@ function operate(operator, firstNumber, lastNumber) {
 }
 
 const digitButtons = document.querySelectorAll(".digitButtons button");
+const operators = document.querySelectorAll(".operators button")
 const outputDisplay = document.querySelector(".outputDisplay")
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+
+outputDisplay.style.font = "140px 'Ariel', sans-serif";
+
+let firstDigit = '';
+let secondDigit = '';
+let currentOperator = '';
+let isOperatorClicked = false;
+let shouldClearDisplay = false;
+let hasChainedOperator = false;
+
+const operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+};
+
 
 digitButtons.forEach(button => {
     button.addEventListener("click", () => {
-        outputDisplay.textContent += button.textContent;
+        if (shouldClearDisplay) {
+            outputDisplay.textContent = '';
+            shouldClearDisplay = false;
+        }
+        outputDisplay.textContent += button.textContent; 
+        if (isOperatorClicked = true) {
+            secondDigit = outputDisplay.textContent;
+            isOperatorClicked = false;
+        };
+    })
+});
+
+operators.forEach(button => {
+    button.addEventListener("click", () => {
+        if (firstDigit && secondDigit) {
+            outputDisplay.textContent = operate(operations[currentOperator], firstDigit, secondDigit);
+            firstDigit = Number(outputDisplay.textContent);
+            secondDigit = '';
+        }
+        else {
+            firstDigit = outputDisplay.textContent;
+        }
+        isOperatorClicked = true; 
+        currentOperator = button.textContent;
+        shouldClearDisplay = true;
     })
 })
 
-console.log(operate(subtract, 5, 4));
-// console.log(subtract(4,5));
-// console.log(multiply(5,4));
-// console.log(divide(5,4));
+equals.addEventListener("click", () => {
+    outputDisplay.textContent = operate(operations[currentOperator], firstDigit, secondDigit);
+})
+
+clear.addEventListener("click", () => {
+    outputDisplay.textContent = '';
+    firstDigit = null;
+    secondDigit = null;
+    currentOperator = null;
+    isOperatorClicked = false;
+    shouldClearDisplay = false;
+});
